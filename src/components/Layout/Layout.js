b5ee65b3f9211e2cdf51a1767fd668072a8563b9
YntyCtrl <yntymak.199848@gmai.com>
2021-03-20 22:35:04 +0600

    create Leyout function

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import classes from "./Layout.module.css";
+import Toolbar from "../Toolbar/Toolbar";
+import Drawer from "../Drawer/Drawer";
+
+const Layout = ({ children }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  return (
+    <div className={classes.Layout}>
+      <Toolbar openDrawer={() => setDrawerOpen(true)} />
+      <Drawer open={drawerOpen} closeDrawer={() => setDrawerOpen(false)} />
+      {children}
+    </div>
+  );
+}
+ 
+export default Layout;
