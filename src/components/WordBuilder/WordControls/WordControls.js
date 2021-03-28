0b83e0dc697deba2682e3ba487b2aab904876450
YntyCtrl <yntymak.199848@gmai.com>
2021-03-28 17:47:09 +0600

    add function to WordControls

---
+++ src/components/WordBuilder/WordControls/WordControls.js
@@ -0,0 +1,11 @@
+import classes from "./WordControls.module.css";
+
+const WordControls = () => {
+    return ( 
+        <div className={classes.WordControls}>
+
+        </div>
+    );
+}
+ 
+export default WordControls;
