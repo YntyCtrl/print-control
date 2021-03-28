f591369bbe2a9b1f1994c547a662246cb0547362
YntyCtrl <yntymak.199848@gmai.com>
2021-03-28 17:47:36 +0600

    WordPreview function

---
+++ src/components/WordBuilder/WordPreview/WordPreviw.js
@@ -0,0 +1,11 @@
+import classes from "./WordPreview.module.css";
+
+const WordPrewiew = () => {
+    return ( 
+        <div className={classes.WordPrewiew}>
+
+        </div>
+    );
+}
+ 
+export default WordPrewiew;
