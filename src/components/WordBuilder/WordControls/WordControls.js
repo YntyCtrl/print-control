a5b09b158753337d7fc114f94d2d94a8943af390
YntyCtrl <yntymak.199848@gmai.com>
2021-04-05 18:10:23 +0600

    Added buttons

--- src/components/WordBuilder/WordControls/WordControls.js
+++ src/components/WordBuilder/WordControls/WordControls.js
@@ -1,10 +1,54 @@
 import classes from "./WordControls.module.css";
 
 const WordControls = () => {
-    return ( 
-        <div className={classes.WordControls}>
+    return (
+      <div className={classes.WordControls}>
+        <div className={classes.Keyboard}>
+          <button id="a">A</button>
+          <button id="b">B</button>
+          <button id="c">C</button>
+          <button id="d">D</button>
+          <button id="e">E</button>
+          <button id="f">F</button>
+          <button id="g">G</button>
+          <button id="h">H</button>
+          <button id="i">I</button>
+          <button id="j">J</button>
+          <button id="k">K</button>
+          <button id="l">L</button>
+          <button id="m">M</button>
+          <button id="n">N</button>
+          <button id="o">O</button>
+          <button id="p">P</button>
+          <button id="q">Q</button>
+          <button id="r">R</button>
+          <button id="s">S</button>
+          <button id="t">T</button>
+          <button id="u">U</button>
+          <button id="v">V</button>
+          <button id="w">W</button>
+          <button id="x">X</button>
+          <button id="y">Y</button>
+          <button id="z">Z</button>
+          <button id="z">,</button>
+          <button id="z">.</button>
+          <button id="z">@</button>
+          <button id="z">:</button>
 
+          <button id="q">0</button>
+          <button id="r">1</button>
+          <button id="s">2</button>
+          <button id="t">3</button>
+          <button id="u">4</button>
+          <button id="v">5</button>
+          <button id="w">6</button>
+          <button id="x">7</button>
+          <button id="y">8</button>
+          <button id="z">9</button>
+          <button id="probel">_</button>
+          <button id="del">del</button>
         </div>
+      </div>
     );
 }
  
