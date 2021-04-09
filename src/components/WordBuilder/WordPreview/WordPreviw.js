c0497f3096222d60976696f4ff020f7c1fbbb478
YntyCtrl <yntymak.199848@gmai.com>
2021-04-09 14:01:08 +0600

    fixed bags

--- src/components/WordBuilder/WordPreview/WordPreviw.js
+++ src/components/WordBuilder/WordPreview/WordPreviw.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import WordControls from "../WordControls/WordControls";
+
+
 import classes from "./WordPreview.module.css";
 
 const WordPreview = ({ output }) => {
-const [result, setResult] = useState("Hello World");
+const result = ("Hello World");
 
 return <div className={classes.WordPreview}>{result}</div>;
 };
