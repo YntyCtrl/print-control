4f49983081c07d2e2141a84d5d8ee5888e52cbea
YntyCtrl <yntymak.199848@gmai.com>
2021-04-09 17:41:10 +0600

    transfer buttons to WordBuilder

--- src/components/WordBuilder/WordBuilder.js
+++ src/components/WordBuilder/WordBuilder.js
@@ -1,13 +1,86 @@
 import classes from "./WordBuilder.module.css";
 import WordControls from "./WordControls/WordControls";
 import WordPrewiew from "./WordPreview/WordPreviw";
+import { useState } from "react";
 
 const WordBuilder = () => {
-    return ( 
-        <div className={classes.WordBuilder} >
-            <WordPrewiew/>
-            <WordControls/>
-        </div> 
+
+    const [output,setOutput] = useState("") ;
+  const buttons = (
+    <div className={classes.Keyboard}>
+      <button
+        onClick={() => setOutput(output + "A")}
+        className={classes.letter}
+      >
+        A
+      </button>
+      <button
+        onClick={() => setOutput(output + "B")}
+        className={classes.letter}
+      >
+        B
+      </button>
+      <button className={classes.letter}>C</button>
+      <button className={classes.letter}>D</button>
+      <button className={classes.letter}>E</button>
+      <button className={classes.letter}>F</button>
+      <button className={classes.letter}>G</button>
+      <button className={classes.letter}>H</button>
+      <button className={classes.letter}>I</button>
+      <button className={classes.letter}>J</button>
+      <button className={classes.letter}>K</button>
+      <button className={classes.letter}>L</button>
+      <button className={classes.letter}>M</button>
+      <button className={classes.letter}>N</button>
+      <button className={classes.letter}>O</button>
+      <button className={classes.letter}>P</button>
+      <button className={classes.letter}>Q</button>
+      <button className={classes.letter}>R</button>
+      <button className={classes.letter}>S</button>
+      <button className={classes.letter}>T</button>
+      <button className={classes.letter}>U</button>
+      <button className={classes.letter}>V</button>
+      <button className={classes.letter}>W</button>
+      <button className={classes.letter}>X</button>
+      <button className={classes.letter}>Y</button>
+      <button className={classes.letter}>Z</button>
+      <button className={classes.symbol}>,</button>
+      <button className={classes.symbol}>.</button>
+      <button className={classes.letter}>@</button>
+      <button className={classes.symbol}>:</button>
+
+      <button className={classes.number}>0</button>
+      <button className={classes.number}>1</button>
+      <button className={classes.number}>2</button>
+      <button className={classes.number}>3</button>
+      <button className={classes.number}>4</button>
+      <button className={classes.number}>5</button>
+      <button className={classes.number}>6</button>
+      <button className={classes.number}>7</button>
+      <button className={classes.number}>8</button>
+      <button className={classes.number}>9</button>
+      <button>_</button>
+      <button>del</button>
+    </div>)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+      <div className={classes.WordBuilder}>
+        <WordPrewiew output={output} />
+        <WordControls buttons={buttons} />
+      </div>
     );
 }
  
