becbaa37f3de7b457052217ba1834698a75458f3
YntyCtrl <yntymak.199848@gmai.com>
2021-04-09 12:37:36 +0600

    create new useState

--- src/components/WordBuilder/WordPreview/WordPreviw.js
+++ src/components/WordBuilder/WordPreview/WordPreviw.js
@@ -1,11 +1,11 @@
+import { useState } from "react";
+import WordControls from "../WordControls/WordControls";
 import classes from "./WordPreview.module.css";
 
-const WordPreview = () => {
-    return ( 
-        <div className={classes.WordPreview}>
-            
-        </div>
-    );
-}
+const WordPreview = ({ output }) => {
+const [result, setResult] = useState("Hello World");
+
+return <div className={classes.WordPreview}>{result}</div>;
+};
  
 export default WordPreview;
